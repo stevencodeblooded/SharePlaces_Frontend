00d719dd27373111a30947dbe9de2ee46e5452a3
stevencodeblooded <steven.ochieng432@gmail.com>
2023-11-06 01:17:37 +0300

    Map Incomplete

--- src/Components/Places/TheMap/Map.jsx
+++ src/Components/Places/TheMap/Map.jsx
@@ -1,25 +1,9 @@
-// import React from 'react'
-
-// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-
-// const Map = withScriptjs(withGoogleMap((props) =>
-//     <GoogleMap
-//         defaultZoom={8}
-//         defaultCenter={{ lat: -34.397, lng: 150.644 }}
-//     >
-//         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-//     </GoogleMap>
-// ))
-
-// export default Map
-
-
-import React from 'react'
+import './Map.css'
 
 const Map = () => {
   return (
-    <div>
-        <h1>The Map Component Goes Here...</h1>
+    <div className='map'>
+      <h1>THE MAP GOES HERE</h1>
     </div>
   )
 }
