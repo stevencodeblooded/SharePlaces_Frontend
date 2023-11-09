b1a70bec14c9a674ab2f3718f4fa4a1a234df917
stevencodeblooded <steven.ochieng432@gmail.com>
2023-11-09 10:11:20 +0300

    Added loading using Suspense, Await, defer and Promise

--- src/Container/Users.jsx
+++ src/Container/Users.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import UserCard from '../Components/Users/UserCard'
-import { useLoaderData } from 'react-router-dom'
+import { Await, defer, useLoaderData } from 'react-router-dom'
 
 export async function loader() {
   const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/users')
-  const data = await response.json()
+  const dataPromise = response.json()
 
-  return data
+  return defer({dataUsers: dataPromise}) //object
 }
 
 const Users = () => {
   const usersData = useLoaderData()
   return (
     <div className='users'>
-      <UserCard users={usersData}/>
+      <Suspense fallback={<h1>Loading Users...</h1>}>
+        <Await resolve={usersData.dataUsers}>
+          {
+            (loadedUsers) => {
+              console.log(loadedUsers);
+              return(
+                <UserCard users={loadedUsers}/>
+              )
+            }
+          }
+        </Await>
+      </Suspense>
     </div>
   )
 }
